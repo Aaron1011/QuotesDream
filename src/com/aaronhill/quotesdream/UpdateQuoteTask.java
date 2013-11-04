e2b4510cd0cb4a5ddcbdee9065c05132c5b861de
Aaron Hill <aa1ronham@gmail.com>
2013-11-04 14:54:11 -0500

    Make author text smaller

--- src/com/aaronhill/quotesdream/UpdateQuoteTask.java
+++ src/com/aaronhill/quotesdream/UpdateQuoteTask.java
@@ -5,6 +5,8 @@ import java.util.List;
 import com.aaronhill.quotesdream.Quote;
 
 import android.os.AsyncTask;
+import android.text.Spannable;
+import android.text.style.AbsoluteSizeSpan;
 import android.util.Log;
 import android.widget.TextView;
 
@@ -31,7 +33,10 @@ public class UpdateQuoteTask extends AsyncTask<Object, Quote, Void> {
 		Quote quote = values[0];
 		Log.d("Progress", "Hi");
 
-		textView.setText(quote.body + "\n--" + quote.author);
+		textView.setText(quote.body + "\n--" + quote.author, TextView.BufferType.SPANNABLE);
+		Spannable str = (Spannable) textView.getText();
+		str.setSpan(new AbsoluteSizeSpan(50, true), 0, str.toString().indexOf("\n"), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+		str.setSpan(new AbsoluteSizeSpan(30, true), str.toString().indexOf("\n") + 1, str.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
 	}
 
 }
