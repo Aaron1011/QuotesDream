250d9d50a2408a59f9339c1accf10ab41f52f3a5
Aaron Hill <aa1ronham@gmail.com>
2013-11-16 08:23:52 -0500

    Use PreferenceFragment

--- src/com/aaronhill/quotesdream/QuotesPreferenceActivity.java
+++ src/com/aaronhill/quotesdream/QuotesPreferenceActivity.java
@@ -2,17 +2,16 @@ package com.aaronhill.quotesdream;
 
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
-import android.app.Activity;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.support.v4.app.NavUtils;
 
 public class QuotesPreferenceActivity extends PreferenceActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		addPreferencesFromResource(R.xml.preferences);
+
+		getFragmentManager().beginTransaction()
+        		.replace(android.R.id.content, new SettingsFragment())
+        		.commit();
 	}
 
 }
