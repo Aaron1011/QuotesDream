dcd18e2cd280011e250a22b58a0c7ce97a9d1315
Aaron Hill <aa1ronham@gmail.com>
2013-11-17 09:15:16 -0500

    Fetch Integer instead of String from SharedPreferences

--- src/com/aaronhill/quotesdream/QuotesDream.java
+++ src/com/aaronhill/quotesdream/QuotesDream.java
@@ -68,7 +68,7 @@ public class QuotesDream extends DreamService {
 		final TextView textView = (TextView) findViewById(R.id.textView1);
 
 		task = new UpdateQuoteTask();
-		task.execute(textView, Integer.parseInt(sharedPref.getString(getString(R.string.quote_display_time), "5")) * 1000);
+		task.execute(textView, sharedPref.getInt(getString(R.string.quote_display_time), 5) * 1000);
 
 	}
 
